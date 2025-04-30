a3f56cc67060a11f8ecae8a5d90c9557987f90d3
uic02628 <uic02628@contiwan.com>
2025-04-30 15:22:17 +0300

    Added the `set_empty_style` method

--- extended_objects/panel_container.gd
+++ extended_objects/panel_container.gd
@@ -38,6 +38,10 @@ func set_style(style: StyleBox) -> void:
 	add_theme_stylebox_override(&"panel", style)
 
 
+func set_empty_style() -> void:
+	add_theme_stylebox_override(&"panel", StyleBoxEmpty.new())
+
+
 func remove_style() -> void:
 	remove_theme_stylebox_override(&"panel")
 #endregion
