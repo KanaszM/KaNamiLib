740ac7842799ca6028a54e0fc7a91f976adae8ac
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-05 09:20:37 +0300

    Added the `ExtendedBoxContainer` class

---
+++ extended_objects/box_container.gd
@@ -0,0 +1,57 @@
+@tool
+class_name ExtendedBoxContainer
+extends BoxContainer
+
+#region Signals
+#endregion
+
+#region Enums
+#endregion
+
+#region Constants
+#endregion
+
+#region Export Variables
+#endregion
+
+#region Public Variables
+#endregion
+
+#region Private Variables
+#endregion
+
+#region OnReady Variables
+#endregion
+
+#region Virtual Methods
+#endregion
+
+#region Pubilc Methods
+#endregion
+
+#region Theme Methods
+func set_separation(value: int) -> void:
+	add_theme_constant_override(&"separation", value)
+
+
+func get_separation() -> int:
+	return get_theme_constant(&"separation")
+#endregion
+
+#region Private Methods
+#endregion
+
+#region Static Methods
+#endregion
+
+#region Signal Callbacks
+#endregion
+
+#region SubClasses
+#endregion
+
+#region Setter Methods
+#endregion
+
+#region Getter Methods
+#endregion
