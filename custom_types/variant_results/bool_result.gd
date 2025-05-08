5b40b8059828b4b2b6d6077bfdf99f31e8fac9ed
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-08 12:20:48 +0300

    Added the `BoolResult` custom type

---
+++ custom_types/variant_results/bool_result.gd
@@ -0,0 +1,68 @@
+#@tool
+class_name BoolResult
+extends VariantResult
+
+#region Signals
+#endregion
+
+#region Enums
+#endregion
+
+#region Constants
+#endregion
+
+#region Export Variables
+#endregion
+
+#region Public Variables
+var value: bool
+#endregion
+
+#region Private Variables
+#endregion
+
+#region OnReady Variables
+#endregion
+
+#region Virtual Methods
+#endregion
+
+#region Public Methods
+func set_value(value_arg: bool, logger_callback: Callable = Callable()) -> BoolResult:
+	value = value_arg
+	
+	if logger_callback.is_valid():
+		Logger.success(logger_callback, value)
+	
+	return self
+
+
+func set_error(
+	error_code_arg: Error, error_message_arg: String = "", logger_callback: Callable = Callable(),
+	) -> BoolResult:
+		error_code = error_code_arg
+		error_message = error_message_arg
+		
+		if error_code != OK and not error_message.is_empty() and logger_callback.is_valid():
+			Logger.error(logger_callback, "%s :: %s" % [error_string(error_code), error_message])
+		
+		return self
+#endregion
+
+#region Private Methods
+#endregion
+
+#region Static Methods
+#endregion
+
+#region Signal Callbacks
+#endregion
+
+#region SubClasses
+#endregion
+
+#region Setter Methods
+#endregion
+
+#region Getter Methods
+#endregion
