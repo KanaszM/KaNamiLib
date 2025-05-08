4a71851ede1e51ae9f5abdc32b1bb6c47dfb32c8
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-08 12:22:37 +0300

    Added methods that verify if the provided vector is a valid direction and methods that calculate travel time

--- utility_scripts/vector.gd
+++ utility_scripts/vector.gd
@@ -27,3 +27,19 @@ static func sorted_clamped_vector2i(vector: Vector2i, min_value: int, max_value:
 	var sorted: Vector2i = sorted_vector2i(vector)
 	
 	return Vector2i(mini(min_value, sorted.x), maxi(max_value, sorted.x))
+
+
+static func vector2_is_directional(vector: Vector2) -> bool:
+	return vector in [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+
+
+static func vector2i_is_directional(vector: Vector2i) -> bool:
+	return vector in [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]
+
+
+static func calculate_vector2_travel_time(position: Vector2, destination: Vector2, speed: float) -> float:
+	return (position - destination).length() / speed
+
+
+static func calculate_vector2i_travel_time(position: Vector2i, destination: Vector2i, speed: float) -> float:
+	return (position - destination).length() / speed
