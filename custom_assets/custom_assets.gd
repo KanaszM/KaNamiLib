ef3f7e2a482e3cb8c5bc99d1b424a910e71fd8a7
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-16 08:38:17 +0300

    Added `custom_assets`

---
+++ custom_assets/custom_assets.gd
@@ -0,0 +1,7 @@
+class_name CustomAssets
+
+#region Images
+const IMG_GRADIENT_BAR_HORIZONTAL: CompressedTexture2D = preload("uid://b5afixrvd7pxr")
+const IMG_GRID_32X_1PX: CompressedTexture2D = preload("uid://cww1l5aaowom3")
+const IMG_PIXEL_WHITE: CompressedTexture2D = preload("uid://d3ju6ghbdrqsl")
+#endregion
