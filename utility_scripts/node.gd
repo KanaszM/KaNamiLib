8ba1017d6bd385c50c6011e1f7a74b05a3fe02ae
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-27 14:39:50 +0300

    On `UtilsNode`, added the `add_child_in_editor` and `recursive_set_owner` methods

--- utility_scripts/node.gd
+++ utility_scripts/node.gd
@@ -25,3 +25,18 @@ static func set_process(node: Node, mode: bool) -> void:
 	node.set_process(mode)
 	node.set_process_input(mode)
 	node.set_physics_process(mode)
+
+
+static func add_child_in_editor(owner: Node, child: Node) -> void:
+	owner.add_child(child)
+	recursive_set_owner(child, owner.get_tree().edited_scene_root)
+
+
+static func recursive_set_owner(child: Node, owner: Node) -> void:
+	child.owner = owner
+	
+	for sub_child: Node in child.get_children():
+		sub_child.owner = owner
+		
+		if sub_child.get_child_count() > 0:
+			recursive_set_owner(sub_child, owner)
