de36c59069381344877df80eaef7a87038804868
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-27 14:44:53 +0300

    Added the `custom_assets` dir and some textures

--- custom_assets/custom_assets.gd
+++ custom_assets/custom_assets.gd
@@ -1,7 +1,10 @@
 class_name CustomAssets
 
-#region Images
-const IMG_GRADIENT_BAR_HORIZONTAL: CompressedTexture2D = preload("uid://b5afixrvd7pxr")
-const IMG_GRID_32X_1PX: CompressedTexture2D = preload("uid://cww1l5aaowom3")
-const IMG_PIXEL_WHITE: CompressedTexture2D = preload("uid://d3ju6ghbdrqsl")
+#region Textures
+const TEXTURE_GRADIENT_BAR_HORIZONTAL: CompressedTexture2D = preload("uid://b5afixrvd7pxr")
+const TEXTURE_GRID_32X_1PX: CompressedTexture2D = preload("uid://cww1l5aaowom3")
+const TEXTURE_GRID_64X_1PX: CompressedTexture2D = preload("uid://cu00jntsmjm4g")
+const TEXTURE_PIXEL_BLACK: CompressedTexture2D = preload("uid://dmnrl43siwow3")
+const TEXTURE_PIXEL_TRANSPARENT: CompressedTexture2D = preload("uid://dd1td7btg02ec")
+const TEXTURE_PIXEL_WHITE: CompressedTexture2D = preload("uid://d3ju6ghbdrqsl")
 #endregion
