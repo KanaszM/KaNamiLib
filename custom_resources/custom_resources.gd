37f564cf21ad138a6324baf813df91c3465c9614
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-05-27 14:43:07 +0300

    Added the `custom_resources` dir + shaders

---
+++ custom_resources/custom_resources.gd
@@ -0,0 +1,16 @@
+class_name CustomResources
+
+#region Shaders
+const SHADER_BCS: Shader = preload("uid://di3s2y0dd2x7a")
+const SHADER_BLUR_GAUSIAN: Shader = preload("uid://bfj6vonn3ln07")
+const SHADER_BLUR_SIMPLE: Shader = preload("uid://buupbgibu2cm7")
+const SHADER_CONTRASTED: Shader = preload("uid://cy7wvj6s04r2o")
+const SHADER_MIRAGE: Shader = preload("uid://c50ujj4fm3c55")
+const SHADER_NEGATIVE: Shader = preload("uid://cvj12vvjfr0xg")
+const SHADER_NORMALIZED: Shader = preload("uid://rlh306r5nnxg")
+const SHADER_OLD_FILM: Shader = preload("uid://ranlvjiyfix8")
+const SHADER_PIXELIZE: Shader = preload("uid://tem6vcadrcdq")
+const SHADER_SEPIA: Shader = preload("uid://r7bcrs8a7jeu")
+const SHADER_VIGNETTE: Shader = preload("uid://djuo4ratwikyg")
+const SHADER_WHIRL: Shader = preload("uid://dv4fj141afx67")
+#endregion
