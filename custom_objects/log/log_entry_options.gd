af81cbe40f52df30bb560a2bee4e764eea2cd0d2
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-06-27 12:36:28 +0300

    Renamed the "Logger*" classes to "Log*" and applied part 2 of the refactoring

---
+++ custom_objects/log/log_entry_options.gd
@@ -0,0 +1,14 @@
+class_name LogEntryOptions
+
+#region Public Variables
+var indent_length: int
+var indent_prefix: String
+
+var placeholder_null: String = "N/A"
+
+var enable_output_logging: bool = true
+var enable_file_logging: bool = true
+
+var output_custom_color: Color = Color.TRANSPARENT
+var output_font_size: int = 14
+#endregion
