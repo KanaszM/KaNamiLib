0fab8de8d0163b82e0494ad0a3d69504941848f8
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-07-27 09:58:10 +0300

    Updated `UtilsNode`
    For the `add_child_in_editor` and `add_child_in_editor_recursive` methods, added option to provide a custom owner node

--- utility_scripts/node.gd
+++ utility_scripts/node.gd
@@ -28,14 +28,14 @@ static func set_process(node: Node, mode: bool) -> void:
 	node.set_physics_process(mode)
 
 
-static func add_child_in_editor(owner: Node, child: Node) -> void:
-	owner.add_child(child)
-	child.owner = owner.get_tree().edited_scene_root
+static func add_child_in_editor(origin: Node, child: Node, custom_owner: Node = null) -> void:
+	origin.add_child(child)
+	child.owner = custom_owner if custom_owner != null else origin.get_tree().edited_scene_root
 
 
-static func add_child_in_editor_recursive(owner: Node, child: Node) -> void:
-	owner.add_child(child)
-	recursive_set_owner(child, owner.get_tree().edited_scene_root)
+static func add_child_in_editor_recursive(origin: Node, child: Node, custom_owner: Node = null) -> void:
+	origin.add_child(child)
+	recursive_set_owner(child, custom_owner if custom_owner != null else origin.get_tree().edited_scene_root)
 
 
 static func recursive_set_owner(child: Node, owner: Node) -> void:
