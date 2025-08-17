a248fea559d84d47271038b276bb25d56a2fffc4
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-08-17 13:10:05 +0300

    Updated `UtilsCallback`
    Added a new function `get_callback_data` that returns the `CallbackData` class

--- utility_scripts/callback.gd
+++ utility_scripts/callback.gd
@@ -30,3 +30,38 @@ static func to_str(
 			]
 		
 		return ("%s%s%s" % [object_str, separator, callback.get_method()])
+
+
+static func get_callback_data(callback: Callable) -> CallbackData:
+	if not callback.is_valid():
+		push_error("The provided callable is not valid!")
+		return null
+	
+	var object: Object = callback.get_object()
+	
+	if object == null:
+		push_error("The callable object could not be identified!")
+		return null
+	
+	if not object is Node:
+		push_error("The callable object must be a Node!")
+		return null
+	
+	var origin := object as Node
+	var method: StringName = callback.get_method()
+	
+	if method.is_empty() or method == &"<anonymous lambda>":
+		push_error("Invalid method name: '%s' on callable: '%s'!" % [method, callback])
+		return null
+	
+	var callback_data: CallbackData = CallbackData.new()
+	
+	callback_data.origin = origin
+	callback_data.method = method
+	
+	return callback_data
+
+
+class CallbackData:
+	var origin: Node
+	var method: StringName
