58bfc242b53990ff88946c8456d5586e34e3f775
KanaszM <kanaszm@yahoo.com>
2025-09-06 10:51:04 +0300

    Removed the `Path` class and it's dependencies

--- custom_objects/path/path_dir_remove_contents_options.gd
+++
@@ -1,8 +0,0 @@
-class_name PathDirRemoveContentsOptions
-
-#region Public Variables
-var skip_files: bool = false
-var skip_dirs: bool = false
-var safe_returns: bool = false
-var send_to_trash: bool = true
-#endregion
