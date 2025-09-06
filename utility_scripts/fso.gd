b7d855e30c8b24d6eea044ad2307a72bba5bbe3e
KanaszM <kanaszm@yahoo.com>
2025-09-06 11:20:07 +0300

    Migrated some function from `Path` to `UtilsFSO`

--- utility_scripts/fso.gd
+++ utility_scripts/fso.gd
@@ -37,3 +37,132 @@ static func get_pack_file_path() -> String:
 
 static func open_externally(file_path: String) -> Error:
 	return OS.shell_open(file_path)
+
+
+static func get_file_contents(path: String, skip_empty_rows: bool = true) -> PackedStringArray:
+	var contents: PackedStringArray
+	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+	
+	if file == null:
+		push_error("File at path: '%s', could not be read! %s" % [path, error_string(FileAccess.get_open_error())])
+		return contents
+	
+	var current_line: String
+	
+	while not file.eof_reached():
+		current_line = file.get_line()
+		
+		if current_line.is_empty() and skip_empty_rows:
+			continue
+		
+		contents.append(current_line)
+	
+	file.close()
+	
+	return contents
+
+
+static func get_file_csv_contents(
+	path: String, skip_empty_rows: bool = true, csv_delimiter: String = ","
+	) -> Array[PackedStringArray]:
+		var contents: Array[PackedStringArray]
+		var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+		
+		if file == null:
+			push_error("File at path: '%s', could not be read! %s" % [path, error_string(FileAccess.get_open_error())])
+			return contents
+		
+		var current_line: PackedStringArray
+		var file_extension: String = path.get_extension()
+		
+		while not file.eof_reached():
+			match file_extension:
+				"csv":
+					current_line = file.get_csv_line(csv_delimiter)
+				"tsv":
+					current_line = file.get_csv_line("\t")
+				_:
+					push_error("Invalid file extension: '%s'! Must be either 'csv' or 'tsv'." % file_extension)
+					return contents
+			
+			if current_line.is_empty() and skip_empty_rows:
+				continue
+			
+			contents.append(current_line)
+		
+		file.close()
+		
+		return contents
+
+
+static func get_name(path: String) -> String:
+	return path.get_file().get_slice(".", 0)
+
+
+static func list_dir(
+	path: String,
+	allow_files: bool = true,
+	allow_dirs: bool = false,
+	recursive: bool = false,
+	limit: int = 0,
+	include_file_extensions: PackedStringArray = PackedStringArray([]),
+	exclude_file_extensions: PackedStringArray = PackedStringArray([]),
+	exclude_file_names: PackedStringArray = PackedStringArray([]),
+	exclude_dir_names: PackedStringArray = PackedStringArray([]),
+	) -> PackedStringArray:
+		var paths: PackedStringArray
+		var dir: DirAccess = DirAccess.open(path)
+		
+		if dir == null:
+			push_error("Dir at path: '%s', could not be opened! %s" % [path, error_string(DirAccess.get_open_error())])
+			return paths
+		
+		dir.list_dir_begin()
+		
+		var current_element: String = dir.get_next()
+		
+		while not current_element.is_empty():
+			var current_path: String = path.path_join(current_element)
+			var current_path_is_valid: bool = true
+			
+			if dir.current_is_dir():
+				if not allow_dirs:
+					current_path_is_valid = false
+				
+				if current_element in exclude_dir_names:
+					current_path_is_valid = false
+				
+				else:
+					if recursive:
+						paths.append_array(list_dir(
+							current_path, allow_files, allow_dirs, recursive, limit,
+							include_file_extensions, exclude_file_extensions, exclude_file_names, exclude_dir_names
+							))
+			
+			else:
+				if not allow_files:
+					current_path_is_valid = false
+				
+				else:
+					var current_path_extension: String = current_path.get_extension()
+					
+					if not include_file_extensions.is_empty():
+						if current_path_extension not in include_file_extensions:
+							current_path_is_valid = false
+					
+					if current_path_extension in exclude_file_extensions:
+						current_path_is_valid = false
+					
+					if not exclude_file_names.is_empty():
+						if get_name(current_path) in exclude_file_names:
+							current_path_is_valid = false
+			
+			if current_path_is_valid:
+				paths.append(current_path)
+				
+				if limit > 0 and paths.size() >= 0:
+					return paths.slice(0, limit)
+			
+			current_element = dir.get_next()
+		
+		return paths
