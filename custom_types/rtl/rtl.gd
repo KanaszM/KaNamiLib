7d8122dc66dad4fad52b043629e31d168e1599ca
Kanasz Mihai <kanaszm@yahoo.com>
2025-12-10 12:24:22 +0200

    It is what it is...

--- custom_types/rtl/rtl.gd
+++ custom_types/rtl/rtl.gd
@@ -12,7 +12,7 @@ var _underline: bool: set = underline
 
 var _size: int = DEFAULT_TEXT_SIZE: set = size
 var _color: Color = Color.TRANSPARENT: set = color
-var _text: String
+var _text: String: set = text
 var _image_size: Vector2i
 var _image_path: String
 
@@ -20,77 +20,12 @@ var _newlines_before: int: set = newlines_before
 var _newlines_after: int: set = newlines_after
 #endregion
 
-#region Constructor
+#region Virtual Methods
 func _init(source: Variant = null) -> void:
 	(_set_as_list if UtilsArray.is_array(source) else _set_as_text).call(source)
 #endregion
 
-#region Public Methods
-func image(image_path: String, width: int = _image_size.x, height: int = _image_size.y) -> RTL:
-	_image_path = image_path
-	_image_size = Vector2i(width, height)
-	
-	return self
-
-
-func newlines_before_and_after(
-	value_before: int = DEFAULT_NEWLINES_COUNT, value_after: int = DEFAULT_NEWLINES_COUNT
-	) -> RTL:
-		_newlines_before = value_before
-		_newlines_after = value_after
-		
-		return self
-
-
-func indent_prefix(prefix: String) -> RTL:
-	return _set_indent(prefix, true)
-
-
-func indent_suffix(prefix: String) -> RTL:
-	return _set_indent(prefix, false)
-
-
-func get_formatted() -> String:
-	var newlines_start: String = "".lpad(_newlines_before, "\n")
-	var newlines_end: String = "".lpad(_newlines_after, "\n")
-	var size_start: String = "[font_size=%d]" % _size
-	var size_end: String = "[/font_size]"
-	var color_start: String = ("[color=#%s]" % _color.to_html()) if _color != Color.TRANSPARENT else ""
-	var color_end: String = "[/color]" if _color != Color.TRANSPARENT else ""
-	var bold_start: String = "[b]" if _bold else ""
-	var bold_end: String = "[/b]" if _bold else ""
-	var underline_start: String = "[u]" if _underline else ""
-	var underline_end: String = "[/u]" if _underline else ""
-	var center_start: String = "[center]" if _center else ""
-	var center_end: String = "[/center]" if _center else ""
-	var text_start_end: String = _text
-	var image_start_end: String = (
-		"" if _image_path.is_empty()
-		else (
-			"[img%s]%s[/img]" % [("=%d" % _image_size.x) if _image_size.y == 0
-			else ("=%dx%d" % [_image_size.x, _image_size.y]), _image_path]
-			)
-		)
-	
-	return "".join(PackedStringArray([
-		newlines_start,
-		size_start,
-		color_start,
-		bold_start,
-		underline_start,
-		center_start,
-		text_start_end,
-		image_start_end,
-		center_end,
-		underline_end,
-		bold_end,
-		color_end,
-		size_end,
-		newlines_end,
-	]))
-#endregion
-
-#region Static Methods
+#region Public Static Methods
 static func append_to(label: RichTextLabel, rtl: RTL, clear: bool = false) -> void:
 	if clear:
 		label.clear()
@@ -150,6 +85,67 @@ static func strip_tags(source: String) -> String:
 	return UtilsRegex.sub(UtilsRegex.SUB_BBCODE, source).strip_edges()
 #endregion
 
+#region Public Methods
+func image(image_path: String, width: int = _image_size.x, height: int = _image_size.y) -> RTL:
+	_image_path = image_path
+	_image_size = Vector2i(width, height)
+	return self
+
+
+func newlines_before_and_after(before: int = DEFAULT_NEWLINES_COUNT, after: int = DEFAULT_NEWLINES_COUNT) -> RTL:
+	_newlines_before = before
+	_newlines_after = after
+	return self
+
+
+func indent_prefix(prefix: String) -> RTL:
+	return _set_indent(prefix, true)
+
+
+func indent_suffix(prefix: String) -> RTL:
+	return _set_indent(prefix, false)
+
+
+func get_formatted() -> String:
+	var newlines_start: String = "".lpad(_newlines_before, "\n")
+	var newlines_end: String = "".lpad(_newlines_after, "\n")
+	var size_start: String = "[font_size=%d]" % _size
+	var size_end: String = "[/font_size]"
+	var color_start: String = ("[color=#%s]" % _color.to_html()) if _color != Color.TRANSPARENT else ""
+	var color_end: String = "[/color]" if _color != Color.TRANSPARENT else ""
+	var bold_start: String = "[b]" if _bold else ""
+	var bold_end: String = "[/b]" if _bold else ""
+	var underline_start: String = "[u]" if _underline else ""
+	var underline_end: String = "[/u]" if _underline else ""
+	var center_start: String = "[center]" if _center else ""
+	var center_end: String = "[/center]" if _center else ""
+	var text_start_end: String = _text
+	var image_start_end: String = (
+		"" if _image_path.is_empty()
+		else (
+			"[img%s]%s[/img]" % [("=%d" % _image_size.x) if _image_size.y == 0
+			else ("=%dx%d" % [_image_size.x, _image_size.y]), _image_path]
+			)
+		)
+	
+	return "".join(PackedStringArray([
+		newlines_start,
+		size_start,
+		color_start,
+		bold_start,
+		underline_start,
+		center_start,
+		text_start_end,
+		image_start_end,
+		center_end,
+		underline_end,
+		bold_end,
+		color_end,
+		size_end,
+		newlines_end,
+	]))
+#endregion
+
 #region Private Methods
 func _to_string() -> String:
 	return get_formatted()
@@ -158,13 +154,11 @@ func _to_string() -> String:
 func _set_as_text(source: Variant) -> RTL:
 	source = str(source)
 	_text = "" if UtilsText.is_empty_or_null(source) else source
-	
 	return self
 
 
 func _set_as_list(source: Array) -> RTL:
 	_text = "[ul]%s[/ul]" % "\n".join(source)
-	
 	return self
 
 
@@ -175,7 +169,6 @@ func _set_indent(character: String, is_prefix: bool) -> RTL:
 		temp_parts.append("%s %s" % [character if is_prefix else part, character if not is_prefix else part])
 	
 	_text = "\n".join(temp_parts)
-	
 	return self
 #endregion
 
@@ -202,6 +195,11 @@ func size(arg: int = DEFAULT_TEXT_SIZE) -> RTL:
 
 func color(arg: Color = Color.TRANSPARENT) -> RTL:
 	_color = arg
+	return self
+
+
+func text(arg: String) -> RTL:
+	_text = arg
 	return self
 
 
--- custom_types/rtl/rtl_sentence.gd
+++ custom_types/rtl/rtl_sentence.gd
@@ -3,23 +3,64 @@ class_name RTLSentence
 #region Public Variables
 var rtls: Array[RTL]
 var join_character: String
+var text_size: int = RTL.DEFAULT_TEXT_SIZE: set = size
 #endregion
 
-#region Constructor
-func _init(rtls_arg: Array[RTL], join_character_arg: String = " ") -> void:
+#region Virtual Methods
+func _init(rtls_arg: Array[RTL] = [], join_character_arg: String = " ") -> void:
 	rtls = rtls_arg
 	join_character = join_character_arg
 #endregion
 
 #region Public Methods
-func append_to(label: RichTextLabel, clear: bool = false) -> void:
+func get_rtls() -> Array[RTL]:
+	return rtls
+
+
+func get_join_character() -> String:
+	return join_character
+
+
+func add_rtl(rtl: RTL) -> RTLSentence:
+	rtls.append(rtl)
+	return self
+
+
+func add_rtls(rtls_arg: Array[RTL]) -> RTLSentence:
+	rtls.append_array(rtls_arg)
+	return self
+
+
+func set_rtl(rtl: RTL) -> RTLSentence:
+	rtls = [rtl]
+	return self
+
+
+func set_rtls(rtls_arg: Array[RTL]) -> RTLSentence:
+	rtls = rtls_arg
+	return self
+
+
+func append_to(output: RichTextLabel, clear: bool = false) -> void:
 	if clear:
-		label.clear()
+		output.clear()
 	
 	var texts: PackedStringArray
 	
 	for rtl: RTL in rtls:
+		rtl.size(text_size)
 		texts.append(str(rtl))
 	
-	label.append_text(join_character.join(texts))
+	output.append_text(join_character.join(texts))
+
+
+func combine(sentence: RTLSentence) -> RTLSentence:
+	rtls.append_array(sentence.rtls)
+	return self
+#endregion
+
+#region Setter Methods
+func size(arg: int = RTL.DEFAULT_TEXT_SIZE) -> RTLSentence:
+	text_size = arg
+	return self
 #endregion
